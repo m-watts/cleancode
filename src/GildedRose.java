201111e6ee13e3912ca83313a6a606794f740c78
Martin Watts <stanly.watts@gmail.com>
2024-11-10 18:55:44 +0100

    Updating task description

--- src/GildedRose.java
+++ src/GildedRose.java
@@ -10,7 +10,9 @@ public class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (items[i].name.equals("Conjured Mana Cake")) {
+                        items[i].quality = items[i].quality - 2;
+                    } else if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
